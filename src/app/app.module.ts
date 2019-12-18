a0452327147661c982b5222ea74c94ef7a5679bc
unknown <sureshkumar.ece15@gmail.com>
2019-12-19 03:01:56 +0530

    SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,15 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { AppModuleModule } from './app-module/app-module.module';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -24,7 +29,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -34,11 +42,13 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppModuleModule
 
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { DishService } from '../services/dish.service';
+import { PromotionService } from '../services/promotion.service';
+import { Promotion } from '../shared/promotion';
+import { Dish } from '../shared/dish';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  dish: Dish;
+  promotion: Promotion;
+
+  constructor(private dishService: DishService, private promotionService: PromotionService) { }
+
+  ngOnInit() {
+    this.dish = this.dishService.getFeaturedDish();
+    this.promotion = this.promotionService.getFeaturedPromotion();
+  }
+
+}
